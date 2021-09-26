49a60f4f39529e1bd17b5bc6427b15511e445494
yumcoder <yumcoder.platform@gmail.com>
2021-09-26 23:27:31 +0330

    add readme context

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Rust by example
 
-Learn Rust by example in yumi way
+Learn Rust by example in yumi way. 
+
+- memory_safety: learn about borrowing, ownership, copy and clone.
+
+- use_borrowed_args: pattern of using a common idioms.
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-mod memory_safety;
-mod use_borrowed_args;
+// mod memory_safety;
+// mod use_borrowed_args;
 
 fn main() {
     println!("rust idioms");
 
-    let str = "hello world";
-    let string = str.to_string();
-    use_borrowed_args::str_string::print_me_string(str.to_string());
-    use_borrowed_args::arr_vec_slice::heap_vec_dynamic_alloc();
+    // let str = "hello world";
+    // let string = str.to_string();
+    // use_borrowed_args::str_string::print_me_string(str.to_string());
+    // use_borrowed_args::arr_vec_slice::heap_vec_dynamic_alloc();
 }
