185554a1ebf9072851a7726c273659cec531f9d3
yumcoder <yumcoder.platform@gmail.com>
2021-10-14 11:06:34 +0330

    ver 1.0.0

--- src/concurrency/task_queue.rs
+++ src/concurrency/task_queue.rs
@@ -1,4 +1,10 @@
-// TODO write a simple task queue
+// An event loop has one or more task queues
+// A task queue is an order list of tasks
+//
+// example: see https://tianpan.co/blog/2015-01-13-understanding-reactor-pattern-for-highly-scalable-i-o-bound-web-server
+//  To handle web requests, there are two competitive web architectures – thread-based one and event-driven one.
+//  The reactor pattern is one implementation technique of the event-driven architecture
+
 mod tests {
     #[test]
     fn test_task_queue() {}
